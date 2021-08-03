3d246c0e3196d227d34357132a26f2105913e9c4
baejimil <qowhdgus629@naver.com>
2021-08-03 17:45:34 +0900

    select test

--- src/main/java/com/bjh/blog/test/DummyControllerTest.java
+++ src/main/java/com/bjh/blog/test/DummyControllerTest.java
@@ -1,6 +1,15 @@
 package com.bjh.blog.test;
 
+import java.util.List;
+import java.util.function.Supplier;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +22,19 @@ public class DummyControllerTest {
 	
 	@Autowired //의존성 주입
 	private UserRepository userRepository;
+	
+	@GetMapping("/dummy/users")
+	public List<User> list() {
+		return userRepository.findAll();
+	}
+	
+	@GetMapping("/dummy/user")
+	public List<User> pageList(@PageableDefault(size=1, sort="id", direction = Sort.Direction.DESC) Pageable pageable) {
+		Page<User> pagingUser = userRepository.findAll(pageable);
+		
+		List<User> users = pagingUser.getContent();
+		return users;
+	}
 	
 	// http://localhost:8000/blog/dummy/join (요청)
 	// http바디에 username, password, email 데이터를 가지고 요
@@ -29,5 +51,28 @@ public class DummyControllerTest {
 		user.setRole(RoleType.USER);
 		userRepository.save(user);
 		return "회원가입이 완료되었습니다.";
+	}
+	
+	@GetMapping("/dummy/join/{id}")
+	public User detail(@PathVariable int id) {
+		//람다식
+//		User user = userRepository.findById(id).orElseThrow(()->{
+//			return new IllegalArgumentException("해당 유저는 없습니다");
+//		});
+		
+		User user = userRepository.findById(id).orElseThrow(new Supplier<IllegalArgumentException>() {
+			@Override
+			public IllegalArgumentException get() {
+				return new IllegalArgumentException("해당 유저는 없습니다 id:"+id);	
+			}
+		});
+		
+//		요청 : 웹브라우저
+//		user 객체 = 자바 오브젝트
+//		변환 (웹브라우저가 이해할 수 있는 데이)-> json (Gson 라이브러리)
+//		스프링부트 = MessageConverter라는 애가 응답시에 자동작동
+//		만약 자바 오브젝트를 리턴하게 되면 Jackson 라이브러리 호출해서
+//		user 오브젝트를 json으로 변환해서 브라우저에 던져
+		return user;
 	}
 }
